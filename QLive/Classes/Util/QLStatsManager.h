2f7075a390c8a5dc94b1dc03c3e96fb01efdd5cd
Sean Yue <tonyfish@qq.com>
2017-03-29 17:12:18 +0800

    Add QLStatsManager

---
+++ QLive/Classes/Util/QLStatsManager.h
@@ -0,0 +1,15 @@
+//
+//  QLStatsManager.h
+//  QLive
+//
+//  Created by Sean Yue on 2017/3/29.
+//  Copyright © 2017年 iqu8. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QLStatsManager : NSObject
+
+QBDeclareSingletonMethod(sharedManager)
+
+@end
---
+++ QLive/Classes/Util/QLStatsManager.m
@@ -0,0 +1,15 @@
+//
+//  QLStatsManager.m
+//  QLive
+//
+//  Created by Sean Yue on 2017/3/29.
+//  Copyright © 2017年 iqu8. All rights reserved.
+//
+
+#import "QLStatsManager.h"
+
+@implementation QLStatsManager
+
+QBSynthesizeSingletonMethod(sharedManager)
+
+@end
